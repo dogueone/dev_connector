deab994ac1fb7ab890ffd74439462107682a5d2f
dogue_one <doggueone@gmail.com>
2019-11-07 13:55:59 +0300

    Profile Action added

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -19,3 +19,46 @@ export const getCurrentProfile = () => async dispatch => {
     });
   }
 };
+
+// Create or update profile
+export const createProfile = (
+  formData,
+  history,
+  edit = false
+) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    const res = await axios.post('/api/profile', formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(
+      setAlert(edit ? 'Profile Upadated' : 'Profile Created', 'success')
+    );
+
+    // To redirect we can't use <Redirect> here, we use withRouter history object
+    if (!edit) {
+      history.push('/dashboard');
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    // from auth action
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
